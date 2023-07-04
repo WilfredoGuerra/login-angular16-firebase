cec5f06acbaec69dce25066b1a2746bc088ffe98
Wilfredo Guerra <willigue@gmail.com>
2023-07-04 16:00:41 -0400

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'user/sing-up', loadChildren: () => import('./pages/users/sing-up/sing-up.module').then(m => m.SingUpModule) }, { path: 'user/sing-in', loadChildren: () => import('./pages/users/sing-in/sing-in.module').then(m => m.SingInModule) }, { path: 'user/profile', loadChildren: () => import('./pages/users/profile/profile.module').then(m => m.ProfileModule) }, { path: 'user/email-verification', loadChildren: () => import('./pages/users/email-verification/email-verification.module').then(m => m.EmailVerificationModule) }, { path: 'user/forgot-password', loadChildren: () => import('./pages/users/forgot-password/forgot-password.module').then(m => m.ForgotPasswordModule) }, { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
